2b3c9017aa0bfad6b911f89daabc9bf57ca7a66c
LUIS EMILIO SIARUQUI MAGDALENO <a224215957@unison.mx>
2025-10-10 17:04:56 -0700

    Consultas

--- ConexionesBD.cs
+++ ConexionesBD.cs
@@ -10,6 +10,14 @@ public class ConexionesBD
               + " Integrated Security=True; "
               + " TrustServerCertificate=True";
     public string ConsultaObtenerAcademico = "SELECT [IdAcademico]\r\n      ,[Nombre]\r\n      ,[Apellidos]\r\n      ,[Grado]\r\n      ,[FechaHoraCreacion]\r\n  FROM [GruposBD].[dbo].[Academico]";
+    public string ConsultaObtenerAlumnos = "SELECT[IdAlumnos],[Nombre],[Apellidos],[Estatus],[FechaHoraCreacion] FROM [GruposBD].[dbo].[Alumnos]";
+    public string ConsultaObtenerAula = "SELECT[IdAula],[Edificio],[Aula],[Piso],[CapacidadMaxima],[FechaHoraCreacion]FROM[GruposBD].[dbo].[Aula]";
+    public string ConsultaObtenerCarrera = "SELECT[IdCarrera],[NombreCarrera],[SiglasCarrera],[FechaHoraCreacion]FROM[GruposBD].[dbo].[Carrera]";
+    public string ConsultaObteneCiudad = "SELECT[IdCiudad],[NombreCiudad],[SiglasCiudad],[FechaHoraCreacion],[IdEstado] FROM [GruposBD].[dbo].[Ciudad]";
+    public string ConsultaObtenerEstado = "SELECT[IdEstado],[NombreEstado],[SiglaEstado],[FechaHoraCreacion],[IdPais]FROM[GruposBD].[dbo].[Estado]";
+    public string ConsultaObtenerMateria = "SELECT[IdMateria],[NombreMateria],[Creditos],[FechaHoraCreacion]FROM[GruposBD].[dbo].[Materia]";
+    public string ConsultaObtenerPais = "SELECT[IdPais],[NombrePais],[SiglaPais],[FechaHoraCreacion]FROM[GruposBD].[dbo].[Pais]";
+
     public void ObtenerBDAcademicos(DataGridView dgv,string consulta) {
         SqlConnection con = new SqlConnection(connexion);
 
